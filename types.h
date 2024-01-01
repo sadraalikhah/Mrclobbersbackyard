054b8fe280e4bab3a1df5cd87f6a949565fc3b9b
sadra <itssadraalikhah@gmail.com>
2024-01-01 22:08:47 +0330

    +Making the code cleaner

--- types.h
+++ types.h
@@ -1,13 +1,31 @@
 #pragma once
+
+// Structure to represent statistics of game entities
 struct stats{
 	int attack;
-	int deffense;
+	int defense;
 	int speed;
 	int points;
 };
 
+// Structure to represent the position and type of a game entity
 struct pos {
 	int x; //position x
 	int y; //position y
 	int type;
+};
+
+struct stats dog[4] = {
+	[0].attack = 5,
+	[0].defense = 30,
+	[0].speed = 5,
+	[1].attack = 2,
+	[1].defense = 15,
+	[1].speed = 2,
+	[2].attack = 2,
+	[2].defense = 20,
+	[2].speed = 3,
+	[3].attack = 1,
+	[3].defense = 1,
+	[3].speed = 1,
 };
