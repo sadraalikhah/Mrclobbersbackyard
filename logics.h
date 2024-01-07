c2f2c3c3ffd6b16e57307e1a0029958a6180e5b5
sadra <itssadraalikhah@gmail.com>
2024-01-07 12:50:23 +0330

    +minor changes

--- logics.h
+++ logics.h
@@ -4,15 +4,15 @@
 
 int isLegal(int y, int x, int move)  //1: up, 2: left, 3: down, 4: right
 {
-	if (y < 0 && y > 15 && x > 0 && x < 15) return 0;
+	if (y < 0 || y > 14 || x < 0 || x > 14) return 0;
 	//move up
-	if (wallH[y-1][x] && move == 1) return 0;
+	if (wallH[y][x] && move == 1) return 0;
 	//move down
-	if (wallH[y - 1][x - 1] && move == 3) return 0;
+	if (wallH[y][x] && move == 3) return 0;
 	//move left
-	if (wallV[y - 1][x] && move == 2) return 0;
+	if (wallV[y][x] && move == 2) return 0;
 	//move right
-	if (wallV[y - 1][x - 1] && move == 4) return 0;
+	if (wallV[y+1][x-1] && move == 4) return 0;
 	return 1;
 }
 
