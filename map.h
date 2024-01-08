3bbf1e1da1fa16dfcfb6809400defc4454f3e167
sadra <itssadraalikhah@gmail.com>
2024-01-08 09:51:05 +0330

    +new method of spawn

--- map.h
+++ map.h
@@ -12,6 +12,7 @@ float distance(struct pos point1, struct pos point2);
 // Check if a position is in the excluded region
 int isExcludedRegion(int x, int y);
 
+
 // Generate walls on the board based on certain conditions
 int generate_walls()
 {
@@ -68,28 +69,76 @@ int generate_walls()
 void spawn(struct pos object[], int n, int sw[][BOARD_SIZE])
 {
     srand(time(NULL));
-    int i, j;
-    float minDist = 30.00 / n;
-    for (int k = 0; k < n;) {
-        do {
-            i = rand() % BOARD_SIZE;
-            j = rand() % BOARD_SIZE;
-            if (sw[i][j] == 1 || ((i >= 6 && i <= 9) && (j >= 6 && j <= 9))) continue;
-
-            object[k].x = j;
-            object[k].y = i;
-            if (k > 1 && distance(object[k], object[k - 1]) < minDist) continue;
-
-            sw[i][j] = 1;
-            k++;
-            
-        } while (k < n || isExcludedRegion(object[k].x, object[k].y));
+    int objectsPerCorner = n / 4;
+    int objectsRemainder = (n % 4)/2;
+    int objectNumber = 0;
+    int y, x;
+    for (int i = 0; i < objectsPerCorner + objectsRemainder; i++)
+    {
+        y = rand() % 7;
+        x = rand() % 7;
+        //duplicate check
+        if (sw[y][x] || isExcludedRegion(y, x))
+        {
+            i--;
+            continue;
+        }
+        object[objectNumber].x = x;
+        object[objectNumber].y = y;
+        objectNumber++;
+        sw[y][x]++;
+    }
+    for (int i = 0; i < objectsPerCorner; i++)
+    {
+        y = rand() % 7;
+        x = rand() % 7+8;
+        //duplicate check
+        if (sw[y][x] || isExcludedRegion(y, x))
+        {
+            i--;
+            continue;
+        }
+        object[objectNumber].x = x;
+        object[objectNumber].y = y;
+        objectNumber++;
+        sw[y][x]++;
+    }
+    for (int i = 0; i < objectsPerCorner; i++)
+    {
+        y = rand() % 7+8;
+        x = rand() % 7;
+        //duplicate check
+        if (sw[y][x] || isExcludedRegion(y, x))
+        {
+            i--;
+            continue;
+        }
+        object[objectNumber].x = x;
+        object[objectNumber].y = y;
+        objectNumber++;
+        sw[y][x]++;
+    }
+    for (int i = 0; i < objectsPerCorner + objectsRemainder; i++)
+    {
+        y = rand() % 7+8;
+        x = rand() % 7+8;
+        //duplicate check
+        if (sw[y][x] || isExcludedRegion(y, x))
+        {
+            i--;
+            continue;
+        }
+        object[objectNumber].x = x;
+        object[objectNumber].y = y;
+        objectNumber++;
+        sw[y][x]++;
     }
 }
 
 float distance(struct pos point1,struct pos point2) {
     return sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
 }
-int isExcludedRegion(int x, int y) {
-    return x >= 3 && x <= 11 && y >= 3 && y <= 11;
+int isExcludedRegion(int y, int x) {
+    return x > 5 && x < 9 && y > 5 && y < 9;
 }
+
